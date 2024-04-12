d42836789ce1b5db86d70bee8371c6044117f9e8
Александр Рак <alexandr.rak.people@gmail.com>
2024-04-12 09:47:15 +0500

    add style

--- src/views/index/index.ts
+++ src/views/index/index.ts
@@ -1,12 +1,13 @@
 import * as PIXI from "pixi.js";
 import * as REVOLT from "revolt-fx";
-
+import "./index.scss";
 
 let app = new PIXI.Application<HTMLCanvasElement>({
   width: 1920,
   height: 1080,
   background: 0x000000
 });
+app.ticker.maxFPS = 30;
 document.getElementById("app").appendChild(app.view);
 
 const fx = new REVOLT.FX();
@@ -20,12 +21,30 @@ PIXI.Assets.load(['fx_settings', 'fx_spritesheet']).then(function (data) {
   const emitter = fx.getParticleEmitter('all');
 
   //Inititialize it with the target PIXI container
-  emitter.init(app.stage);
+  emitter.init(new PIXI.Container());
   emitter.start();
-  emitter.x = 960;
-  emitter.y - 540;
-  app.ticker.add(function () {
+  emitter.container.x = 960;
+  emitter.container.y = 260;
+  emitter.container.setParent(app.stage);
+  app.ticker.add(function (delta) {
     //Update the RevoltFX instance
-    fx.update();
+    fx.update(delta);
   });
 });
+
+function resize() {
+  const scaleX = window.innerWidth / 1920;
+  const scaleY = window.innerHeight / 1080;
+  let ratio = Math.min(scaleX, scaleY);
+  let dx = window.innerWidth - ratio * 1920;
+  let dy = window.innerHeight - ratio * 1080;
+
+  app.view.setAttribute("style", `
+    width: ${ratio * 1920}px;
+    height: ${ratio * 1080}px;
+    left: ${dx > 0 ? ~~(dx / 2) : 0}px;
+    top:  ${dy > 0 ? ~~(dy / 2) : 0}px;
+  `);
+}
+resize();
+window.addEventListener("resize", () => resize());
